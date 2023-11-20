ec15f0b8114af8ae02e8ea05bc13ac6a17b8a444
tim <tim@timdev.de>
2023-11-20 13:59:58 +0100

    refactor(exam04): refactor to get rid of warnings

--- exam04/Raten.java
+++ exam04/Raten.java
@@ -1,3 +1,5 @@
+package exam04;
+
 import java.util.*;
 
 /**
@@ -7,24 +9,24 @@ public class Raten {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter amount of tries:");
+            final int tries = sc.nextInt();
 
-        System.out.println("Enter amount of tries:");
-        final int tries = sc.nextInt();
+            final int secretNumber = ((int)(Math.random() * 1000)) + 1;
 
-        final int secretNumber = ((int)(Math.random() * 1000)) + 1;
+            // boolean hasWon = false;
 
-        boolean hasWon = false;
-
-        for (int i = 0; i < tries; i++) {
-            System.out.println("Enter your next guess:");
-            int guess = sc.nextInt();
-            if (guess == secretNumber) {
-                hasWon = true;
-                System.out.println("You correctly guessed the secret number " + secretNumber);
-                break;
+            for (int i = 0; i < tries; i++) {
+                System.out.println("Enter your next guess:");
+                int guess = sc.nextInt();
+                if (guess == secretNumber) {
+                    // hasWon = true;
+                    System.out.println("You correctly guessed the secret number " + secretNumber);
+                    break;
+                }
+                System.out.println("The number you guessed is too " + ((guess < secretNumber) ? "small." : "big."));
             }
-            System.out.println("The number you guessed is too " + ((guess < secretNumber) ? "small." : "big."));
         }
 
         System.out.println("You ran out of guesses. Better luck next time!");
